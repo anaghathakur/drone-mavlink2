697b6e2d729cd8a8e699aef2db11ba8132efb3af
VedantS73 <vedantsawant72003@gmail.com>
2024-10-04 12:57:26 +0530

    Refactor arm_guided.py to establish GPS fix before setting home location and print status messages

--- arm_guided.py
+++ arm_guided.py
@@ -1,15 +1,25 @@
 from dronekit import connect, VehicleMode, APIException
 import time
+
 def establishConnection():
     baud_rate = 57600
     vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=baud_rate)
-
     return vehicle
 
 def set_home_location():
     """
     Check and set the home location for the vehicle.
     """
+    print("Waiting for GPS fix...")
+    
+    # Wait for the GPS to get a good fix (i.e., number of satellites > 5)
+    while vehicle.gps_0.fix_type < 2:  # 2 means GPS has a valid fix
+        print(" Waiting for GPS fix... current fix type:", vehicle.gps_0.fix_type)
+        time.sleep(1)
+    
+    print("GPS fix acquired!")
+
+    # Wait for home location to be set
     while not vehicle.home_location:
         print("Waiting for home location to be set...")
         cmds = vehicle.commands
@@ -17,7 +27,7 @@ def set_home_location():
         cmds.wait_ready()
         time.sleep(1)
 
-    # If home location is still not set, set it manually to current location
+    # If home location is still not set, set it manually to the current location
     if not vehicle.home_location:
         print("Setting home location to the current location...")
         vehicle.home_location = vehicle.location.global_frame
@@ -28,7 +38,6 @@ def arm_and_takeoff(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
-
     print("Basic pre-arm checks")
     # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
@@ -46,14 +55,13 @@ def arm_and_takeoff(aTargetAltitude):
         time.sleep(1)
 
     print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
+    # Wait until the vehicle reaches a safe height
     while True:
         print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        #Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
             print("Reached target altitude")
             break
         time.sleep(1)
